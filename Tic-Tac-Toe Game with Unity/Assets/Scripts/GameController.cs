7a70be500f54ea9d9f58195be3cdfb08bea2982e
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-01 16:05:04 +0300

    Added Game Initialize function

--- Tic-Tac-Toe Game with Unity/Assets/Scripts/GameController.cs
+++ Tic-Tac-Toe Game with Unity/Assets/Scripts/GameController.cs
@@ -5,10 +5,31 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public int whoseTurn; // 0 = x and 1 = o
+    public int turnCount;
+    public GameObject[] turnIcons;
+    public Sprite[] playerIcons;
+    public Button[] tictactoeSpaces; //playable space for out game
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameInitialize();
+    }
+
+    void GameInitialize()
+    {
+        whoseTurn = 0;
+        turnCount = 0;
+        turnIcons[0].SetActive(true);
+        turnIcons[1].SetActive(false);
+
+        for(int i = 0; i < tictactoeSpaces.Length; i++)
+        {
+            tictactoeSpaces[i].interactable = true;
+            tictactoeSpaces[i].GetComponent<Image>().sprite = null;
+        }
     }
 
     // Update is called once per frame
